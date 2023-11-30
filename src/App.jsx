53d6831e7edd3a097421cf4fec6b0f9e211e968b
Daniel <daniel.kochovski12@gmail.com>
2023-11-30 21:43:28 +0100

    created SearchParams for location searching

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import createRoot from 'react-dom';
-import Pet from "./Pet";
+import {createRoot} from 'react-dom/client';
+import SearchParams from './SearchParams';
 
 const App = () => {
+  return(
   <div>
     <h1>Adopt Me!</h1>
-    <Pet name="Casper" animal="Cat" breed="Mix" />
-    <Pet name="Marley" animal="Dog" breed="Labrador" />
-    <Pet name="Loki" animal="Dog" breed="Mix" />
+    <SearchParams />
   </div>
+  );
 };
 
 const container = document.getElementById("root");
